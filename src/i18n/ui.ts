197bb3e603ecd143106cce15722b914672d0cbb9
Rafael Sanabria Cruz <rafael.sanabria@es.logicalis.com>
2024-03-04 16:25:27 +0100

    Traducciones para el Hero

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,8 +1,12 @@
 export const ui = {
     en: {
-      
+        'hero.a.email.title': "Send e-mail to {name} at {email}",
+        'hero.a.phone.title': "Call {name} at {phone}",
+        'hero.a.social.title': "Visit {name}'s profile on {network}"
     },
     es: {
-      
+        'hero.a.email.title': "Enviar un correo electrónico a {name} al correo {email}",
+        'hero.a.phone.title': "Llamar por teléfono a {name} al número {phone}",
+        'hero.a.social.title': "Visitar el perfil de {name} en {network}"
     },
   } as const;
